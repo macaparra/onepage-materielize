5159c7572ec756a40631fbd8432d14613c4bf297
Macarena <parrameca@gmail.com>
2022-11-22 14:40:17 -0400

    First commit

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+  
+      $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
+  
+      $(document).ready(function(){
+        $('.materialbox').materialbox();
+        
+      });
+  
+    });
+  })(jQuery);
